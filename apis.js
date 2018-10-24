0c31d35f3853baf3b6994adf22890ede6779b52d
trace-rt <jared_mcklemurry@tamu.edu>
2018-10-24 16:19:34 -0500

    Heatmap test
    Testing Google Maps heatmap overlay.  Moved JS API setup to apis.js

---
+++ apis.js
@@ -0,0 +1,70 @@
+//Firebase
+var config = {
+	apiKey: "AIzaSyC0ORRsYb0e3H-I2IAz-qBbgqInm-Ra3sI",
+	authDomain: "vehicle-blackbox.firebaseapp.com",
+	databaseURL: "https://vehicle-blackbox.firebaseio.com",
+	projectId: "vehicle-blackbox",
+	storageBucket: "vehicle-blackbox.appspot.com",
+	messagingSenderId: "1033879747763"
+};
+firebase.initializeApp(config);
+
+//Google SignIn
+var googleUser = {};
+var startApp = function() {
+gapi.load('auth2', function(){
+  // Retrieve the singleton for the GoogleAuth library and set up the client.
+  auth2 = gapi.auth2.init({
+	client_id: '618088048289-6ojbgiu7u1pc0vl0rkh04kes8mb6ijh8.apps.googleusercontent.com',
+	cookiepolicy: 'single_host_origin',
+	// Request scopes in addition to 'profile' and 'email'
+	//scope: 'additional_scope'
+  });
+});
+};
+
+function onSignIn()
+{
+}
+
+//Google Maps
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+var map, infoWindow;
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 30.61, lng: -96.35},
+  zoom: 13
+});
+infoWindow = new google.maps.InfoWindow;
+
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+	var pos = {
+	  lat: position.coords.latitude,
+	  lng: position.coords.longitude
+	};
+
+	infoWindow.setPosition(pos);
+	infoWindow.setContent('Location found.');
+	infoWindow.open(map);
+	map.setCenter(pos);
+  }, function() {
+	handleLocationError(true, infoWindow, map.getCenter());
+  });
+} else {
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
+}
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+infoWindow.setPosition(pos);
+infoWindow.setContent(browserHasGeolocation ?
+					  'Error: The Geolocation service failed.' :
+					  'Error: Your browser doesn\'t support geolocation.');
+infoWindow.open(map);
+}
--- script.js
+++ script.js
@@ -19,6 +19,8 @@ $(document).ready(function()
 	});
 
 //page event callbacks
+var map, heatmapLayer;
+
 function setMapOverlay()
 {
 	switch($("#select-overlay option:selected").text())
@@ -31,12 +33,37 @@ function setMapOverlay()
 			break;
 		case "Heatmap":
 			$("#select-route").hide(200);
+			populateHeatmap(firebase.database().ref("/vehicles/_TEST_"));
+			heatmapLayer = new google.maps.visualization.HeatmapLayer({
+				data: heatmap,
+				map: map
+        });
+			
 	}
 }
 
 function setMapRoute()
 {
 	//TEMP use array index of option value for selecting route data
+}
+
+//overlay functions
+var heatmap = [];
+
+function populateHeatmap(vehicleRef)
+{
+	/*vehicleRef.once(‘value’, function (snap) {
+ snap.forEach(function (childSnap) {
+  console.log(‘user’, childSnap.val());
+ });
+});*/
+//heatmap.push(new google.maps.LatLng(37.782551, -122.445368));
+
+	var temp = [30.6123, -96.3351, 30.6138, -96.3342, 30.6142, -96.3328];
+	for(var i = 0; i < 3; i++)
+	{
+		heatmap.push(new google.maps.LatLng(temp[i * 2], temp[i * 2 + 1));
+	}
 }
 
 //UI guide
@@ -54,76 +81,3 @@ var x2 = div2.offset().left + (div2.width()/2);
 var y2 = div2.offset().top + (div2.height()/2);
 
 line.attr('x1',x1).attr('y1',y1).attr('x2',x2).attr('y2',y2);
-
-
-
-//Firebase
- var config = {
-    apiKey: "AIzaSyC0ORRsYb0e3H-I2IAz-qBbgqInm-Ra3sI",
-    authDomain: "vehicle-blackbox.firebaseapp.com",
-    databaseURL: "https://vehicle-blackbox.firebaseio.com",
-    projectId: "vehicle-blackbox",
-    storageBucket: "vehicle-blackbox.appspot.com",
-    messagingSenderId: "1033879747763"
-  };
-  //firebase.initializeApp(config);
-
-//Google SignIn
-var googleUser = {};
-var startApp = function() {
-gapi.load('auth2', function(){
-  // Retrieve the singleton for the GoogleAuth library and set up the client.
-  auth2 = gapi.auth2.init({
-	client_id: '618088048289-6ojbgiu7u1pc0vl0rkh04kes8mb6ijh8.apps.googleusercontent.com',
-	cookiepolicy: 'single_host_origin',
-	// Request scopes in addition to 'profile' and 'email'
-	//scope: 'additional_scope'
-  });
-});
-};
-
-function onSignIn()
-{
-}
-
-//Google Maps
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-var map, infoWindow;
-function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 30.61, lng: -96.35},
-  zoom: 13
-});
-infoWindow = new google.maps.InfoWindow;
-
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-	var pos = {
-	  lat: position.coords.latitude,
-	  lng: position.coords.longitude
-	};
-
-	infoWindow.setPosition(pos);
-	infoWindow.setContent('Location found.');
-	infoWindow.open(map);
-	map.setCenter(pos);
-  }, function() {
-	handleLocationError(true, infoWindow, map.getCenter());
-  });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
-}
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-infoWindow.setPosition(pos);
-infoWindow.setContent(browserHasGeolocation ?
-					  'Error: The Geolocation service failed.' :
-					  'Error: Your browser doesn\'t support geolocation.');
-infoWindow.open(map);
-}
