f25c9f522baeaac648f356e4e231c29d2a461cf0
trace-rt <jared_mcklemurry@tamu.edu>
2018-11-13 21:48:29 -0600

    Fixed Chart & Started BG Animation
    Data chart metrics now displayable. Fixed selection handling to choose chart. Started work on background chevron animation WIP.

--- data.js
+++ data.js
@@ -3,12 +3,7 @@ var margin = { top: 20, right: 20, bottom: 20, left: 20 },
 		width = 1000 - margin.left - margin.right,
 		height = 500 - margin.top - margin.bottom;
 
-function setRoutes(routes)
-{
-	rs = routes;
-}
-
-function drawChart(select, rKey)
+function drawChart(select, routeKey)
 {
 	//clear div
 	$(".chart").html("");
@@ -22,22 +17,31 @@ function drawChart(select, rKey)
 	switch(select)
 	{
 		case 0:
-			metrics();
+			metrics(routeKey);
 			break;
 		case 1:
 			colorTest();
 	}
 	
-	function metrics()
+	function metrics(rKey)
 	{
-		var data = [{ name: "Speed", labelOffset: 20, value: function(t) { return rs[rKey][t].speed || 0; }},
-			{ name: "RPMs", labelOffset: 40, value: function(t) { return rs[rKey][t].rpm || 0; }},
-			{ name: "Engine Load", labelOffset: 60, value: function(t) { return rs[rKey][t].load || 0; }},
-			{ name: "Intake Pressure", labelOffset: 80, value: function(t) { return rs[rKey][t].intake || 0; }},
-			{ name: "Engine Temp", labelOffset: 100, value: function(t) { return rs[rKey][t].temp || 0; }}];
+		var len = rs[rKey].length;
+		var intp = d3.scaleSequential(d3.interpolatePiYG);
+		var data = [{ name: "Speed", labelOffset: 20, metric: "speed", value: intp },
+			{ name: "RPMs", labelOffset: 40, metric: "rpm", value: intp },
+			{ name: "Engine Load", labelOffset: 60, metric: "load", value: intp },
+			{ name: "Intake Pressure", labelOffset: 80, metric: "intake", value: intp },
+			{ name: "Engine Temp", labelOffset: 100, metric: "temp", value: intp }]
+			.map(function(m)
+			{
+				return m.plot = d3.range(len).map(function(t)
+				{
+					return { x: t, y: rs[rKey][t][m.metric] || 0 }
+				}), m;
+			});
 		
 		var x = d3.scaleLinear()
-			.domain([0, rs[rKey].length])
+			.domain([0, len - 1])
 			.range([0, width]);
 
 		var y = d3.scaleLinear()
@@ -48,8 +52,8 @@ function drawChart(select, rKey)
 
 		var line = d3.line()
 			.curve(d3.curveBasis)
-			.x(function(d) { return x(d.input); })
-			.y(function(d) { return y(d.delta); });
+			.x(function(d) { return x(d.x); })
+			.y(function(d) { return y(d.y); });
 
 		svg.append("g")
 			.attr("class", "axis axis--x")
@@ -67,20 +71,20 @@ function drawChart(select, rKey)
 			.text("Color Difference at ±10° (CIE76)");
 
 		var g = svg.selectAll(".line")
-			.data(data1)
+			.data(data)
 			.enter()
 			.append("g")
 			.attr("class", "line");
 
 		g.append("path")
-			.attr("d", function(d) { return line(d.deltas); })
+			.attr("d", function(d) { return line(d.plot); })
 			.attr("id", function(d, i) { return "path-" + i; })
 			.style("stroke", function(d, i) { return colors[i]; });
 
 		g.append("text")
 			.attr("x", function(d) { return d.labelOffset; })
 			.attr("dy", -5)
-			.style("fill", function(d, i) { return d3.lab(z[i]).darker(); })
+			.style("fill", function(d, i) { return d3.lab(colors[i]).darker(); })
 			.append("textPath")
 			.attr("class", "textpath")
 			.attr("xlink:href", function(d, i) { return "#path-" + i; })
@@ -131,7 +135,7 @@ function drawChart(select, rKey)
 			.text("Color Difference at ±10° (CIE76)");
 
 		var g = svg.selectAll(".line")
-			.data(data1)
+			.data(data)
 			.enter()
 			.append("g")
 			.attr("class", "line");
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-//dynamic page content
 var dest = "";
 var routes = []
 var heatmap = [];
 var map, heatmapLayer;
+var curRoute = 0;
 
+//dynamic page content
 function fadeBegin()
 {
 	$(dest).hide().css("visibility", "visible").fadeIn(200);
@@ -13,16 +14,14 @@ function fadeBegin()
 $(document).ready(function()
 	{
 		$("nav a").click(function()
-			{
-				dest = $(this).attr("href");
-				$("content").fadeOut(200, fadeBegin).css("visibility", "hidden");
-				return false;  
-			});
+		{
+			dest = $(this).attr("href");
+			$("content").fadeOut(200, fadeBegin).css("visibility", "hidden");
+			return false;  
+		});
 		
 		$("#select-route").hide();
-		populateHeatmap(firebase.database().ref("/vehicles/_TEST_"));
-		setRoutes(routes);
-		drawChart(0, "route0");
+		pullData(firebase.database().ref("/vehicles/_TEST_"));
 	});
 
 //page event callbacks
@@ -32,6 +31,36 @@ function googleAPIReady()
 	heatmapLayer.setMap(null);
 }
 
+function pullData(vehicleRef)
+{
+	vehicleRef.once("value", function(snap)
+	{
+		snap.forEach(function(routeSnap)
+		{
+			//name of DB directory
+			var rKey = routeSnap.key;
+			if(rKey == "info") return;
+			
+			routes[rKey] = [];
+			routeSnap.forEach(function(pSnap)
+			{
+				p = pSnap.val();
+				routes[rKey].push(p);
+				heatmap.push(new google.maps.LatLng(p.lat, p.lng));
+			})
+			
+			$("#select-route").append("<option>" + rKey + "</option>");
+			$("#select-data-route").append("<option>" + rKey + "</option>");
+		})
+	});
+	rs = routes;
+	setTimeout(function()
+		{
+			drawChart(0, "route" + curRoute);
+		}, 500);
+}
+
+//overlay functions
 function setMapOverlay()
 {
 	switch($("#select-overlay option:selected").text())
@@ -55,44 +84,26 @@ function setMapRoute()
 	//TEMP use array index of option value for selecting route data
 }
 
+//chart functions
 function setDataChart()
 {
 	switch($("#select-chart option:selected").text())
 	{
 		case "Route Metrics":
 			$("#select-data-route").show(200);
+			drawChart(0, "route" + curRoute);
+			break;
 		case "Test":
 			$("#select-data-route").hide(200);
+			drawChart(1);
 	}
 }
 
-//overlay functions
-function populateHeatmap(vehicleRef)
+function setDataRoute()
 {
-	vehicleRef.once("value", function(snap)
-	{
-		snap.forEach(function(routeSnap)
-		{
-			//name of DB directory
-			var rKey = routeSnap.key;
-			if(rKey == "info") return;
-			
-			routes[rKey] = [];
-			routeSnap.forEach(function(pSnap)
-			{
-				p = pSnap.val();
-				routes[rKey].push(p);
-				heatmap.push(new google.maps.LatLng(p.lat, p.lng));
-			})
-			
-			$("#select-route").append("<option>" + rKey + "</option>");
-			$("#select-data-route").append("<option>" + rKey + "</option>");
-		})
-	});
+	curRoute = $("#select-chart option:selected").index();
+	drawChart(0, "route" + curRoute);
 }
-
-//chart functions
-
 
 //UI guide
 /*var bodyRect = 0, // document.body.getBoundingClientRect(),
@@ -106,3 +117,121 @@ var y1 = div1.offset().top + (div1.height()/2);
 var x2 = div2.offset().left + (div2.width()/2);
 var y2 = div2.offset().top + (div2.height()/2);
 line.attr('x1',x1).attr('y1',y1).attr('x2',x2).attr('y2',y2);*/
+
+//background animation
+(function ($, window)
+{
+	function Pattern(canvas, options)
+	{
+		var $canvas = $(canvas),
+			context = canvas.getContext("2d"),
+			img = new Image(),
+			defaults = {
+				chev: {
+					color: "rgba(255, 255, 255, .5)",
+					width: 1
+				},
+				position: {
+					x: canvas.width * 1,
+					y: canvas.height * 0.5
+				},
+				width: window.innerWidth,
+				height: window.innerHeight,
+				velocity: 0.4,
+				length: 60,
+				distance: 200,
+				radius: 150,
+				chevs: []
+			},
+			config = $.extend(true, {}, defaults, options);
+
+		function Chevron()
+		{
+			this.x = Math.random() * (canvas.width + 200) - 100;
+			this.y = Math.random() * (canvas.height + 200) - 100;
+			this.z = Math.random() * 100;
+		}
+
+		Chevron.prototype = {
+			create: function()
+			{
+				context.drawImage(img, this.position.x, this.position.y);
+			},
+
+			animate: function()
+			{
+				for(var i = 0; i < config.length; i++)
+				{
+					var chev = config.chevs[i];
+
+					if(chev.y > canvas.height + 100)
+					{
+						chev.y = -100;
+					}
+					
+					chev.y += config.velocity;
+				}
+			}
+		};
+
+		this.createChevs = function()
+		{
+			var length = config.length, chev, i;
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			
+			for(i = 0; i < length; i++)
+			{
+				config.chevs.push(new Chevron());
+				chev = config.chevs[i];
+				chev.create();
+			}
+			chev.animate();
+		};
+
+		this.setCanvas = function()
+		{
+			canvas.width = $("#canL").width();
+			canvas.height = config.height;
+		};
+
+		this.loop = function(callback)
+		{
+			callback();
+			window.requestAnimationFrame(function() { this.loop(callback); }.bind(this));
+		};
+
+		this.bind = function()
+		{
+			$(window).on('mousemove', function(e)
+			{
+				config.position.x = e.pageX - $canvas.offset().left;
+				config.position.y = e.pageY - $canvas.offset().top;
+			});
+		};
+
+		this.init = function()
+		{
+			this.setCanvas();
+			this.img.src = "resources/chevron.svg";
+			this.loop(this.createChevs);
+			this.bind();
+		};
+	}
+
+	$.fn.initBG = function(options)
+	{
+		return this.each(function()
+		{
+			var c = new Pattern(this, options);
+			c.init();
+		});
+	};
+})
+($, window);
+$("#canL").initBG(
+	{
+		chev: {
+			width: 1
+		},
+		radius: 350
+	});
